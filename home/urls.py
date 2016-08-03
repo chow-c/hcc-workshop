3930a83c1be047684e7fc1dc583a63b62e595e50
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-08-03 22:43:14 +1000

    Enable basic user registration

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from . import views
 
@@ -7,4 +9,5 @@ urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
     url(r'^home$', views.Home.as_view(), name='home'),
     url(r'^about$', views.About.as_view(), name='about'),
+    url(r'^register', CreateView.as_view(template_name='home/register.html',form_class=UserCreationForm,success_url='/home'), name='register'),
 ]
