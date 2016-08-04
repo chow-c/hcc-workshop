d241c17992032c192d0d6a41150290bd291bacaf
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-08-04 21:56:49 +1000

    Add cybersecurity templates

--- cybersecurity/views.py
+++ cybersecurity/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
+class Index(generic.TemplateView):
+    template_name = 'cybersecurity/index.html'
+
+def privacy(request):
+    return render(request,'cybersecurity/privacy.html')
+
+def phishing(request):
+    return render(request,'cybersecurity/phishing.html')
