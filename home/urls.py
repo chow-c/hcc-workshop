e69ee89b6a30230959ae46383a96254062733c5c
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-08-07 15:40:07 +1000

    Add basic PDF generation

--- home/urls.py
+++ home/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
     url(r'^home$', views.Home.as_view(), name='home'),
     url(r'^about$', views.About.as_view(), name='about'),
-    url(r'^register', CreateView.as_view(template_name='home/register.html',form_class=UserCreationForm,success_url='/home'), name='register'),
+    url(r'^register$', CreateView.as_view(template_name='home/register.html',form_class=UserCreationForm,success_url='/home'), name='register'),
+    url(r'^pdf$', views.generate_webpage, name='pdf'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,11 @@ from django.views import generic
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+from reportlab.pdfgen import canvas
+
+from io import BytesIO
 
 # Create your views here.
 class Index(generic.TemplateView):
@@ -15,3 +20,26 @@ class Home(generic.TemplateView):
 
 class About(generic.TemplateView):
     template_name = 'home/about.html'
+
+def generate_webpage(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    buffer = BytesIO()
+
+    # Create the PDF object, using the BytesIO object as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw PDF
+    p.drawString(100, 100, "Hello world.")
+
+    # Close the PDF object cleanly.
+    p.showPage()
+    p.save()
+
+    # Get the value of the BytesIO buffer and write it to the response.
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
