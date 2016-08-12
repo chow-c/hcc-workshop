ad522570701f47094683f61c62a09cb24b62a19c
leanaco <leanaco@gmail.com>
2016-08-12 16:44:06 +1000

    Save eyetracking data to DB

--- reading_tracking/urls.py
+++ reading_tracking/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'reading_tracking'
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^eyegaze/$', views.eyegaze, name='eyegaze'),
 ]
--- reading_tracking/views.py
+++ reading_tracking/views.py
@@ -1,15 +1,28 @@
 ## views.py
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views import generic
+from .forms import ReadingEyegazeForm
 
-from .models import ReadingEyegaze
+# from .models import ReadingEyegaze
 
 # Create your views here.
 class Index(generic.TemplateView):
     template_name = 'reading_tracking/index.html'
 
 def index(request):
-
+    if request.method == 'POST':
+        form = ReadingEyegazeForm(request.POST)
+        if form.is_valid():
+            eyegaze_form = form.save(commit=False)
+            eyegaze_form.user = request.user
+            eyegaze_form.save()
+            return HttpResponseRedirect('/reading_tracking/eyegaze')
+    else:
+        form = ReadingEyegazeForm()
+    
+    return render(request,'reading_tracking/index.html', {'form':form})
+    
 
 def eyegaze(request):
     return render(request,'reading_tracking/eyegaze.html')
