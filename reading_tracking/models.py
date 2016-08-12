6129844b94607ffb0627b3d604edba1b5492d292
leanaco <leanaco@gmail.com>
2016-08-12 12:32:18 +1000

    Add image on dashboard and gaze info page

--- reading_tracking/models.py
+++ reading_tracking/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+def ReadingEyegaze(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    gazedata = models.TextField()
--- reading_tracking/views.py
+++ reading_tracking/views.py
@@ -2,9 +2,14 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import ReadingEyegaze
+
 # Create your views here.
 class Index(generic.TemplateView):
     template_name = 'reading_tracking/index.html'
+
+def index(request):
+
 
 def eyegaze(request):
     return render(request,'reading_tracking/eyegaze.html')
