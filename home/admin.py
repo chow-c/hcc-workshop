f45a10f7c6673c09546e2e5558a899c763cbc877
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-08-12 13:25:23 +1000

    Modify newsletter admin view

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import NewsletterSignup
 
-admin.site.register(NewsletterSignup)
+class NewsletterAdmin(admin.ModelAdmin):
+    readonly_fields = ('signup_date',)
+    list_display = ('email', 'signup_date')
+
+admin.site.register(NewsletterSignup, NewsletterAdmin)
--- home/models.py
+++ home/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from django.utils.timezone import now
+
+
 # Create your models here.
 class NewsletterSignup(models.Model):
 
     email = models.EmailField()
+    signup_date = models.DateTimeField('Date Signed Up',auto_now_add=True)
 
     def __str__(self):
         return self.email
