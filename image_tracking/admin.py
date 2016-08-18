8be6dec795a046b62af02ed183add81e42aaedfc
leanaco <leanaco@gmail.com>
2016-08-18 14:24:14 +1000

    Fix record and draw fixations in Reading Tracking

--- image_tracking/admin.py
+++ image_tracking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ImageEyegaze
+
 # Register your models here.
+class ImageEyegazeAdmin(admin.ModelAdmin):
+    readonly_fields = ('timestamp','id')
+    list_display = ('user','timestamp','id')
+
+admin.site.register(ImageEyegaze,ImageEyegazeAdmin)
