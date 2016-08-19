fd2bd999a3800116889d81a84a700b306cb857f6
leanaco <leanaco@gmail.com>
2016-08-19 10:02:41 +1000

    Include GIF for join the dots

--- join_the_dots/urls.py
+++ join_the_dots/urls.py
@@ -7,5 +7,5 @@ app_name = 'join_the_dots'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^eyegaze/$', views.eyegaze, name='eyegaze'),
-    url(r'^dots_gif/$', views.dots_gif, name='dots_gif'),
+    # url(r'^dots_gif/$', views.dots_gif, name='dots_gif'),
 ]
