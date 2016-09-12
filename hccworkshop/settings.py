75cd13a6cbf2aca8720838d818f7505ec19d8fe8
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-09-12 20:44:26 +1000

    Clean up static folders

--- hccworkshop/settings.py
+++ hccworkshop/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +39,6 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'cybersecurity.apps.CybersecurityConfig',
     'thatcher.apps.ThatcherConfig',
-    'other_research.apps.OtherResearchConfig',
     'reading_tracking.apps.ReadingTrackingConfig',
     'image_tracking.apps.ImageTrackingConfig',
     'join_the_dots.apps.JoinTheDotsConfig',
