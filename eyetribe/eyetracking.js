450f45197236d229e34c7eb113fff7ce4ba2ed16
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-09-12 19:25:27 +1000

    Remove old eyetribe files

--- eyetribe/eyetracking.js
+++
@@ -1,9 +0,0 @@
-// Note that the path doesn't matter for routing; any WebSocket
-// connection gets bumped over to WebSocket consumers
-socket = new WebSocket("ws://" + window.location.host + "/chat/");
-socket.onmessage = function(e) {
-    alert(e.data);
-}
-socket.onopen = function() {
-    socket.send("hello world");
-}
--- eyetribe/urls.py
+++
@@ -1,9 +0,0 @@
-## urls.py
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'eyetribe'
-urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
-]
