7c2e6290b86f24f2aec4ba49622ce779c60570b1
chikean <christopher.chow@outlook.com>
2016-09-16 09:29:49 +1000

    Add user levels

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
 
 # Create your models here.
 class NewsletterSignup(models.Model):
@@ -10,3 +13,28 @@ class NewsletterSignup(models.Model):
 
     def __str__(self):
         return self.email
+
+class WorkshopUser(models.Model):
+    LEVELS = (
+    ('0', 'Baby'),
+    ('1', 'Student'),
+    ('2', 'Teacher'),
+    ('3', 'Doctor'),
+    ('4', 'Professor'),
+    ('5', 'Mad Scientist'),
+    ('6', 'Superhero'),
+    ('7', 'Alien'),
+    ('8', 'Wizard'),
+    ('9', 'A.I.'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.CharField(choices=LEVELS,max_length=100,default=0)
+
+    def __unicode__(self):
+        return self.user
+
+def create_workshop_user(sender, instance, created, **kwargs):
+    if created:
+        WorkshopUser.objects.create(user=instance)
+
+post_save.connect(create_workshop_user,sender=User)
