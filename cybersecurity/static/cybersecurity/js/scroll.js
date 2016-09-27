8de5973ec99c9f6a139e02593fec0c8a90014bd6
Christopher Chikean Chow <christopher.chow@anu.edu.au>
2016-09-27 13:16:13 +1000

    Fix basic phishing game

--- cybersecurity/static/cybersecurity/js/scroll.js
+++ cybersecurity/static/cybersecurity/js/scroll.js
@@ -1,5 +1,17 @@
 $(function() {
-  $('a[href*="#phishing-experiment"]:not([href="#"])').click(function() {
+  $('a[href*="#phishing-one"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    $('a[href*="#phishing-experiment"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
