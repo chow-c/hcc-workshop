42b6c39886840ea8e93c2e14556f5834ab8ab84b
chikean <christopher.chow@outlook.com>
2016-12-15 19:19:43 +1100

    Configure celery

--- hccworkshop/celeryconf.py
+++ hccworkshop/celeryconf.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery  
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hccworkshop.settings")
 
@@ -9,5 +8,10 @@ app = Celery('hccworkshop')
 
 CELERY_TIMEZONE = 'UTC'
 
-app.config_from_object('django.conf:settings')  
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)  
+app.config_from_object('django.conf:settings')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
