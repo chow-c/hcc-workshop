8afeb081d58fffc61d73796370e95fcd1d344a3e
chikean <christopher.chow@outlook.com>
2016-12-15 20:32:17 +1100

    Add basic email functionality

---
+++ home/email.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_email():
+
+    subject, from_email, to = 'TestTemplate', 'ANU HCC Workshop <***REMOVED***>', '***REMOVED***'
+
+    text_content = 'Test'
+
+    html_content = render_to_string('home/email_test.html')
+
+    email = EmailMultiAlternatives(subject, text_content, from_email, [to], reply_to=['***REMOVED***'])
+    email.attach_alternative(html_content, "text/html")
+
+    return email.send(fail_silently=False)
